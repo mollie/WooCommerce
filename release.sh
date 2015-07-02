7fd5bd1efb706fac1ff4a46823811f58db793cbf
Lennard van Gunst <lennard@mollie.nl>
2015-07-02 15:00:14 +0200

    Update release.sh to include the new plugin files

--- release.sh
+++ release.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-zip -9 -r mollie-woocommerce-x.x.x.zip MPM/ readme.mdown -x *.git* *.DS_Store
+zip -9 -r woocommerce-mollie-payments-x.x.x.zip woocommerce-mollie-payments.php assets/ i18n/ includes/ readme.mdown LICENSE -x *.git* *.DS_Store

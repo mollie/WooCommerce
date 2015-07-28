c9abe9b2a4edee3ce02c302489f077b2409226b7
Lennard van Gunst <lennard@mollie.nl>
2015-07-28 12:03:17 +0200

    Update release.sh. Ignore .git and .DS_Store

--- release.sh
+++ release.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-zip -9 -r woocommerce-mollie-payments-x.x.x.zip woocommerce-mollie-payments/
+zip -9 -r woocommerce-mollie-payments-x.x.x.zip woocommerce-mollie-payments/ -x *.git* *.DS_Store

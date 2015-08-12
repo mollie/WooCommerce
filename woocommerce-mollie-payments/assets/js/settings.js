fd4bda53babeb9c191bb51de369ffb1a49bf570a
Lennard van Gunst <lennard@mollie.nl>
2015-08-12 11:19:05 +0200

    Make Test API key field required if test mode is enabled.

--- woocommerce-mollie-payments/assets/js/settings.js
+++ woocommerce-mollie-payments/assets/js/settings.js
@@ -2,11 +2,11 @@ jQuery(function($) {
     $('#woocommerce-mollie-payments_test_mode_enabled').change(function() {
         if ($(this).is(':checked'))
         {
-            $('#woocommerce-mollie-payments_test_api_key').closest('tr').show();
+            $('#woocommerce-mollie-payments_test_api_key').attr('required', true).closest('tr').show();
         }
         else
         {
-            $('#woocommerce-mollie-payments_test_api_key').closest('tr').hide();
+            $('#woocommerce-mollie-payments_test_api_key').removeAttr('required').closest('tr').hide();
         }
     }).change();
 });

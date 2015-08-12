66988feb35a25916eafd15a32f6d79a54526ce86
Lennard van Gunst <lennard@mollie.nl>
2015-08-12 14:45:43 +0200

    Update plugin slug in settings.js

--- mollie-payments-for-woocommerce/assets/js/settings.js
+++ mollie-payments-for-woocommerce/assets/js/settings.js
@@ -1,12 +1,12 @@
 jQuery(function($) {
-    $('#woocommerce-mollie-payments_test_mode_enabled').change(function() {
+    $('#mollie-payments-for-woocommerce_test_mode_enabled').change(function() {
         if ($(this).is(':checked'))
         {
-            $('#woocommerce-mollie-payments_test_api_key').attr('required', true).closest('tr').show();
+            $('#mollie-payments-for-woocommerce_test_api_key').attr('required', true).closest('tr').show();
         }
         else
         {
-            $('#woocommerce-mollie-payments_test_api_key').removeAttr('required').closest('tr').hide();
+            $('#mollie-payments-for-woocommerce_test_api_key').removeAttr('required').closest('tr').hide();
         }
     }).change();
 });

555bef5b1010de399742a0075ab0b7043a78255f
Lennard van Gunst <lennard@mollie.nl>
2015-08-17 10:46:28 +0200

    Do not update submodules in deploy script

--- deploy.sh
+++ deploy.sh
@@ -94,11 +94,6 @@ echo "Clear $SVNPATH"
 rm -fr $SVNPATH/
 
 echo
-echo "Update GIT submodules
-git submodule init
-git submodule update
-
-echo
 echo "Creating local copy of SVN repo trunk ..."
 svn checkout $SVNURL $SVNPATH --depth immediates
 svn update --quiet $SVNPATH/trunk --set-depth infinity

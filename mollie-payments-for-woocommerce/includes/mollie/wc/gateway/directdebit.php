ddd4df2590feabaf48e8837b4f84a19820577aab
davdebcom <david@davdeb.com>
2017-09-06 11:31:59 +0200

    SEPA Direct Debit: add settings description, remove irrelevant checkout options (in WooCommerce settings)

--- mollie-payments-for-woocommerce/includes/mollie/wc/gateway/directdebit.php
+++ mollie-payments-for-woocommerce/includes/mollie/wc/gateway/directdebit.php
@@ -1,73 +1,74 @@
 <?php
-class Mollie_WC_Gateway_DirectDebit extends Mollie_WC_Gateway_Abstract
-{
-	/**
-	 * @return string
-	 */
-	public function getMollieMethodId ()
-	{
-		return Mollie_API_Object_Method::DIRECTDEBIT;
-	}
 
+class Mollie_WC_Gateway_DirectDebit extends Mollie_WC_Gateway_Abstract {
 	/**
 	 * @return string
 	 */
-	public function getDefaultTitle ()
-	{
-		return __('SEPA Direct Debit', 'mollie-payments-for-woocommerce');
+	public function getMollieMethodId() {
+		return Mollie_API_Object_Method::DIRECTDEBIT;
 	}
 
 	/**
 	 * Initialise Gateway Settings Form Fields
 	 */
-	public function init_form_fields()
-	{
+	public function init_form_fields() {
 		parent::init_form_fields();
 
-		unset($this->form_fields['title']);
-		unset($this->form_fields['display_logo']);
-		unset($this->form_fields['description']);
+		unset( $this->form_fields['title'] );
+		unset( $this->form_fields['display_logo'] );
+		unset( $this->form_fields['description'] );
 
 	}
 
 	/**
 	 * @return string
 	 */
-	protected function getDefaultDescription ()
-	{
-		return __('SEPA Direct Debit is used for recurring payments with WooCommerce Subscriptions, and will not be shown in the WooCommerce checkout for regular payments!', 'mollie-payments-for-woocommerce');
+	public function getDefaultTitle() {
+		return __( 'SEPA Direct Debit', 'mollie-payments-for-woocommerce' );
 	}
 
-    /**
-     * {@inheritdoc}
-     *
-     * @return bool
-     */
-    protected function paymentConfirmationAfterCoupleOfDays ()
-    {
-        return true;
-    }
+	/**
+	 * @return string
+	 */
+	protected function getSettingsDescription() {
+		return __( 'SEPA Direct Debit is used for recurring payments with WooCommerce Subscriptions, and will not be shown in the WooCommerce checkout for regular payments! You also need to enable iDEAL is you want to use SEPA Direct Debit.', 'mollie-payments-for-woocommerce' );
+	}
+
+	/**
+	 * @return string
+	 */
+	protected function getDefaultDescription() {
+		return '';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 *
+	 * @return bool
+	 */
+	protected function paymentConfirmationAfterCoupleOfDays() {
+		return true;
+	}
 
 	/**
 	 * @param WC_Order                  $order
 	 * @param Mollie_API_Object_Payment $payment
 	 * @param bool                      $admin_instructions
 	 * @param bool                      $plain_text
+	 *
 	 * @return string|null
 	 */
-	protected function getInstructions (WC_Order $order, Mollie_API_Object_Payment $payment, $admin_instructions, $plain_text)
-	{
-		if ($payment->isPaid() && $payment->details)
-		{
+	protected function getInstructions( WC_Order $order, Mollie_API_Object_Payment $payment, $admin_instructions, $plain_text ) {
+		if ( $payment->isPaid() && $payment->details ) {
 			return sprintf(
 			/* translators: Placeholder 1: consumer name, placeholder 2: consumer IBAN, placeholder 3: consumer BIC */
-				__('Payment completed by <strong>%s</strong> (IBAN: %s, BIC: %s)', 'mollie-payments-for-woocommerce'),
+				__( 'Payment completed by <strong>%s</strong> (IBAN: %s, BIC: %s)', 'mollie-payments-for-woocommerce' ),
 				$payment->details->consumerName,
-				implode(' ', str_split($payment->details->consumerAccount, 4)),
+				implode( ' ', str_split( $payment->details->consumerAccount, 4 ) ),
 				$payment->details->consumerBic
 			);
 		}
 
-		return parent::getInstructions($order, $payment, $admin_instructions, $plain_text);
+		return parent::getInstructions( $order, $payment, $admin_instructions, $plain_text );
 	}
 }

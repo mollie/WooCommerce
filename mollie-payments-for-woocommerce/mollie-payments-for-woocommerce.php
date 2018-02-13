091d667f149d9c88068f541cac35ed5042126e81
davdebcom <david@davdeb.com>
2018-02-13 13:54:10 +0100

    Only show one compatibility warning at a time

--- mollie-payments-for-woocommerce/mollie-payments-for-woocommerce.php
+++ mollie-payments-for-woocommerce/mollie-payments-for-woocommerce.php
@@ -97,7 +97,7 @@ function mollie_wc_plugin_inactive() {
 	}
 
 	if ( ! is_admin() ) {
-		return;
+		return false;
 	}
 
 	if ( ! is_plugin_active( 'woocommerce/woocommerce.php' ) ) {
@@ -105,6 +105,7 @@ function mollie_wc_plugin_inactive() {
 		echo '<div class="error"><p>';
 		echo sprintf( esc_html__( '%1$sMollie Payments for WooCommerce is inactive.%2$s The %3$sWooCommerce plugin%4$s must be active for it to work. Please %5$sinstall & activate WooCommerce &raquo;%6$s', 'mollie-payments-for-woocommerce' ), '<strong>', '</strong>', '<a href="https://wordpress.org/plugins/woocommerce/">', '</a>', '<a href="' . esc_url( admin_url( 'plugins.php' ) ) . '">', '</a>' );
 		echo '</p></div>';
+		return false;
 	}
 
 	if ( version_compare( get_option( 'woocommerce_db_version' ), '2.2', '<' ) ) {
@@ -112,6 +113,7 @@ function mollie_wc_plugin_inactive() {
 		echo '<div class="error"><p>';
 		echo sprintf( esc_html__( '%1$sMollie Payments for WooCommerce is inactive.%2$s This version requires WooCommerce 2.2 or newer. Please %3$supdate WooCommerce to version 2.2 or newer &raquo;%4$s', 'mollie-payments-for-woocommerce' ), '<strong>', '</strong>', '<a href="' . esc_url( admin_url( 'plugins.php' ) ) . '">', '</a>' );
 		echo '</p></div>';
+		return false;
 
 	}
 }

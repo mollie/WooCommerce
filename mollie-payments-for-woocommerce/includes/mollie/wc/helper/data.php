763e74c974f12b8b3d487a11e5f592687c7c3ec4
davdebcom <david@davdeb.com>
2018-04-12 14:59:25 +0200

    Fallback for getUserMollieCustomerId, get Mollie Customer ID from recent subscription, closes #202

--- mollie-payments-for-woocommerce/includes/mollie/wc/helper/data.php
+++ mollie-payments-for-woocommerce/includes/mollie/wc/helper/data.php
@@ -570,6 +570,25 @@ class Mollie_WC_Helper_Data
 	    } else {
 		    $customer    = new WC_Customer( $user_id );
 		    $customer_id = $customer->get_meta( 'mollie_customer_id' );
+
+		    // If there is no Mollie Customer ID set, check the most recent active subscription
+		    if ( empty( $customer_id ) ) {
+
+			    $customer_latest_subscription = wc_get_orders( array (
+				    'limit'    => 1,
+				    'customer' => $user_id,
+				    'type'     => 'shop_subscription',
+				    'status'   => 'wc-active',
+			    ) );
+
+			    if ( ! empty( $customer_latest_subscription ) ) {
+				    $customer_id = get_post_meta( $customer_latest_subscription[0]->get_id(), '_mollie_customer_id', $single = true );
+
+				    // Store this customer ID as user meta too
+				    $this->setUserMollieCustomerId( $user_id, $customer_id );
+			    }
+
+		    }
 	    }
 
 	    // If there is a Mollie Customer ID set, check that customer ID is valid for this API key

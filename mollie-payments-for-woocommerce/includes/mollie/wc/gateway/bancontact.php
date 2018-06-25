c021bfe7d452f0a695d9e6087552ca83f0413110
davdebcom <david@davdeb.com>
2018-06-25 14:10:57 +0200

    Formatted according to WordPress coding standards

--- mollie-payments-for-woocommerce/includes/mollie/wc/gateway/bancontact.php
+++ mollie-payments-for-woocommerce/includes/mollie/wc/gateway/bancontact.php
@@ -2,43 +2,39 @@
 
 use Mollie\API\Types\PaymentMethod;
 
-class Mollie_WC_Gateway_Bancontact extends Mollie_WC_Gateway_AbstractSepaRecurring
-{
-    /**
-     *
-     */
-    public function __construct ()
-    {
-        $this->supports = array(
-            'products',
-            'refunds',
-        );
+class Mollie_WC_Gateway_Bancontact extends Mollie_WC_Gateway_AbstractSepaRecurring {
+	/**
+	 *
+	 */
+	public function __construct() {
+		$this->supports = array (
+			'products',
+			'refunds',
+		);
 
-        // If there are still old MisterCash settings, copy them to Bancontact and remove MisterCash.
-	    $mistercash_settings = get_option( 'mollie_wc_gateway_mistercash_settings' );
-	    if ( $mistercash_settings != false ) {
-		    add_option( 'mollie_wc_gateway_bancontact_settings', $mistercash_settings );
-		    delete_option( 'mollie_wc_gateway_mistercash_settings' );
-	    }
+		// If there are still old MisterCash settings, copy them to Bancontact and remove MisterCash.
+		$mistercash_settings = get_option( 'mollie_wc_gateway_mistercash_settings' );
+		if ( $mistercash_settings != false ) {
+			add_option( 'mollie_wc_gateway_bancontact_settings', $mistercash_settings );
+			delete_option( 'mollie_wc_gateway_mistercash_settings' );
+		}
 
-        parent::__construct();
-    }
+		parent::__construct();
+	}
 
-    /**
-     * @return string
-     */
-    public function getMollieMethodId ()
-    {
-        return PaymentMethod::BANCONTACT;
-    }
+	/**
+	 * @return string
+	 */
+	public function getMollieMethodId() {
+		return PaymentMethod::BANCONTACT;
+	}
 
-    /**
-     * @return string
-     */
-    public function getDefaultTitle ()
-    {
-        return __('Bancontact', 'mollie-payments-for-woocommerce');
-    }
+	/**
+	 * @return string
+	 */
+	public function getDefaultTitle() {
+		return __( 'Bancontact', 'mollie-payments-for-woocommerce' );
+	}
 
 	/**
 	 * @return string
@@ -48,10 +44,9 @@ class Mollie_WC_Gateway_Bancontact extends Mollie_WC_Gateway_AbstractSepaRecurri
 	}
 
 	/**
-     * @return string
-     */
-    protected function getDefaultDescription ()
-    {
-        return '';
-    }
+	 * @return string
+	 */
+	protected function getDefaultDescription() {
+		return '';
+	}
 }

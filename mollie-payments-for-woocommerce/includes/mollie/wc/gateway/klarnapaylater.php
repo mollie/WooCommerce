e550dc53292dc8147ddc32e6743af9aa5c680c23
davdebcom <david@davdeb.com>
2018-09-26 20:48:35 +0200

    Klarna: removed getInstructions until more is known about Klarna response contents

--- mollie-payments-for-woocommerce/includes/mollie/wc/gateway/klarnapaylater.php
+++ mollie-payments-for-woocommerce/includes/mollie/wc/gateway/klarnapaylater.php
@@ -47,28 +47,4 @@ class Mollie_WC_Gateway_KlarnaPayLater extends Mollie_WC_Gateway_Abstract
 	{
 		return '';
 	}
-
-	/**
-	 * @param WC_Order                  $order
-	 * @param Mollie\Api\Resources\Order $mollie_order
-	 * @param bool                      $admin_instructions
-	 * @param bool                      $plain_text
-	 * @return string|null
-	 */
-	protected function getInstructions (WC_Order $order, Mollie\Api\Resources\Order $mollie_order, $admin_instructions, $plain_text)
-	{
-		if ($payment->isPaid() && $payment->details)
-		{
-			return sprintf(
-			/* translators: Placeholder 1: PayPal consumer name, placeholder 2: PayPal email, placeholder 3: PayPal transaction ID */
-				__("Payment completed by <strong>%s</strong> - %s (PayPal transaction ID: %s)", 'mollie-payments-for-woocommerce'),
-				$payment->details->consumerName,
-				$payment->details->consumerAccount,
-				$payment->details->paypalReference
-			);
-		}
-
-		return parent::getInstructions($order, $payment, $admin_instructions, $plain_text);
-
-	}
 }

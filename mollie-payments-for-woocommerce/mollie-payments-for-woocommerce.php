b14165a6170ee71f6bcf01faa5aa3367414be7f1
davdebcom <david@davdeb.com>
2018-10-11 13:59:50 +0200

    Update version and changelog for 5.0.1 release

--- mollie-payments-for-woocommerce/mollie-payments-for-woocommerce.php
+++ mollie-payments-for-woocommerce/mollie-payments-for-woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name: Mollie Payments for WooCommerce
  * Plugin URI: https://www.mollie.com
  * Description: Accept payments in WooCommerce with the official Mollie plugin
- * Version: 5.0.0
+ * Version: 5.0.1
  * Author: Mollie
  * Author URI: https://www.mollie.com
  * Requires at least: 3.8

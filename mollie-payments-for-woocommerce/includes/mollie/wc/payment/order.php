82be748940ba5afa28e0c6b710874619dce148bc
davdebcom <david@davdeb.com>
2019-02-14 11:52:52 +0100

    Extended refund processing v7: support order liens with amount == 0

--- mollie-payments-for-woocommerce/includes/mollie/wc/payment/order.php
+++ mollie-payments-for-woocommerce/includes/mollie/wc/payment/order.php
@@ -823,10 +823,17 @@ class Mollie_WC_Payment_Order extends Mollie_WC_Payment_Object {
 		Mollie_WC_Plugin::debug( 'Try to process individual order item refunds or cancels.' );
 
 		// Try to do the actual refunds or cancellations
+
+		// Loop through items in the WooCommerce refund
 		foreach ( $items as $key => $item ) {
 
-			Mollie_WC_Plugin::debug( 'Item: ' . $item );
+			// Some merchants update orders with an order line with value 0, in that case skip processing that order line.
+			$item_refund_amount_precheck = abs( $item->get_total() + $item->get_total_tax() );
+			if ( $item_refund_amount_precheck == 0 ) {
+				continue;
+			}
 
+			// Loop through items in the Mollie payment object (Order)
 			foreach ( $payment_object->lines as $line ) {
 
 				// If there is no metadata wth the order item ID, this order can't process individual order lines
@@ -851,7 +858,7 @@ class Mollie_WC_Payment_Order extends Mollie_WC_Payment_Object {
 				if ( $original_order_item_id == $line->metadata->order_item_id ) {
 
 					// Mollie doesn't allow a partial refund of less than 1 quantity, so when merchants try that, warn them and block the process
-					if ( number_format( $line->totalAmount->value, 2) > number_format( $item_refund_amount, 2) ) {
+					if ( number_format( $line->unitPrice->value, 2) > number_format( $item_refund_amount, 2) ) {
 
 						$note_message = sprintf( "Mollie doesn't allow a partial refund of less than 1 quantity per order line. Use 'Refund amount' instead. The Mollie order line (%s) unit price is %s, and the WooCommerce refund item (%s) amount is %s.",
 							$line->id,

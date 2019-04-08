7ad71f3b8a2b739dab2c6ef5ed86a866d61c6800
davdebcom <david@davdeb.com>
2019-04-08 21:18:51 +0200

    Update "WooCommerce tested up to" to WooCommerce 3.6

--- mollie-payments-for-woocommerce/mollie-payments-for-woocommerce.php
+++ mollie-payments-for-woocommerce/mollie-payments-for-woocommerce.php
@@ -12,7 +12,7 @@
  * Domain Path: /i18n/languages/
  * License: GPLv2 or later
  * WC requires at least: 2.2.0
- * WC tested up to: 3.5
+ * WC tested up to: 3.6
  */
 
 // Exit if accessed directly.

50a920ff080aac38321eaca8fe7624bc02e77839
carmenmaymo <mayen22@gmail.com>
2020-05-18 18:03:27 +0200

    Add svg cc methods to cc class
    MOL-115

--- src/Mollie/WC/Gateway/Creditcard.php
+++ src/Mollie/WC/Gateway/Creditcard.php
@@ -103,4 +103,79 @@ class Mollie_WC_Gateway_Creditcard extends Mollie_WC_Gateway_AbstractSubscriptio
 
         $this->form_fields = array_merge($this->form_fields, $fields);
     }
+
+    /**
+     * @return array Array containing the credit cards names enabled in settings
+     *               to make customization of checkout icons
+     */
+    protected function enabledCreditcards()
+    {
+        $creditCardSettings = get_option('mollie_wc_gateway_creditcard_settings', false) ?: [];
+        $enabled = isset($creditCardSettings[Mollie_WC_Helper_PaymentMethodsIconUrl::MOLLIE_CREDITCARD_ICONS_ENABLER])
+            ? wc_string_to_bool($creditCardSettings[Mollie_WC_Helper_PaymentMethodsIconUrl::MOLLIE_CREDITCARD_ICONS_ENABLER])
+            : false;
+
+        if (!$enabled) {
+            return [];
+        }
+        $optionLexem = Mollie_WC_Helper_PaymentMethodsIconUrl::MOLLIE_CREDITCARD_ICONS;
+        $creditcardsAvailable = Mollie_WC_Helper_PaymentMethodsIconUrl::AVAILABLE_CREDITCARD_ICONS;
+        $svgFileName = Mollie_WC_Helper_PaymentMethodsIconUrl::SVG_FILE_EXTENSION;
+        $enabledCreditcards = [];
+
+        $creditcardSettings = get_option('mollie_wc_gateway_creditcard_settings');
+        foreach ($creditcardsAvailable as $card) {
+            if ($creditcardSettings[$optionLexem . $card] === 'yes') {
+                $enabledCreditcards[] = $card . $svgFileName;
+            }
+        }
+
+        return $enabledCreditcards;
+    }
+
+    /**
+     *
+     * @return string Newly composed svg string
+     */
+    public function buildSvgComposed()
+    {
+        $enabledCreditCards = $this->enabledCreditcards();
+        $assetsImagesPath
+            = '/app/public/wp-content/plugins/WooCommerce-Mollie/public/images/Creditcard_issuers/';
+        $cardWidth = Mollie_WC_Helper_PaymentMethodsIconUrl::CREDIT_CARD_ICON_WIDTH;
+        $cardsNumber = count($enabledCreditCards);
+        $cardsWidth = $cardWidth * $cardsNumber;
+        $cardPositionX = 0;
+        $actual
+            = "<svg width=\"{$cardsWidth}\" height=\"24\" style=\"float:right\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">";
+        foreach ($enabledCreditCards as $creditCard) {
+            $actual .= $this->positionSvgOnX(
+                $cardPositionX,
+                file_get_contents(
+                    $assetsImagesPath . $creditCard
+                )
+            );
+            $cardPositionX += $cardWidth;
+        }
+        $actual .= "</svg>";
+
+        return $actual;
+    }
+
+    /**
+     * Method to add the x parameter to the svg string so that the icon can
+     * be positioned related to other icons.
+     *
+     * @param int    $xPosition coordinate to position icon on x axis
+     * @param string $svgString svg string to add position to
+     *
+     * @return string|string[] Modified svg string with the x position added
+     */
+    protected function positionSvgOnX($xPosition, $svgString)
+    {
+        $positionString = " x=\"{$xPosition}\"";
+        $positionAfterSvgWord = 4;
+
+        return substr_replace($svgString, $positionString, $positionAfterSvgWord, 0);
+    }
 }
--- src/Mollie/WC/Helper/PaymentMethodsIconUrl.php
+++ src/Mollie/WC/Helper/PaymentMethodsIconUrl.php
@@ -42,13 +42,6 @@ class Mollie_WC_Helper_PaymentMethodsIconUrl
      */
     public function svgUrlForPaymentMethod($paymentMethodName)
     {
-        if ($paymentMethodName == PaymentMethod::CREDITCARD
-            && $this->enabledCreditcards()
-            && !is_admin()
-        ) {
-            return $this->composeSvgImage($this->enabledCreditcards());
-        }
-
         return isset($this->paymentMethodImages[$paymentMethodName]->svg)
             ? $this->paymentMethodImages[$paymentMethodName]->svg
             : $this->fallToAssets($paymentMethodName);
@@ -69,118 +62,6 @@ class Mollie_WC_Helper_PaymentMethodsIconUrl
         return Mollie_WC_Plugin::getPluginUrl(
             "public/images/{$paymentMethodName}" . self::SVG_FILE_EXTENSION
         );
-    }
-
-    /**
-     * Method that handles the composition of the new composed svg file
-     * and returns its url
-     *
-     * @param array $enabledCreditCards Array with the enabled creditcards
-     *
-     * @return string the url to the composed svg
-     */
-    protected function composeSvgImage($enabledCreditCards)
-    {
-        $this->writeToSvgFile();
-
-        $actual = $this->buildSvgComposed($enabledCreditCards);
-
-        return  $this->writeToSvgFile($actual);
-    }
-
-    /**
-     * @return array Array containing the credit cards names enabled in settings
-     *               to make customization of checkout icons
-     */
-    protected function enabledCreditcards()
-    {
-        $creditCardSettings = get_option('mollie_wc_gateway_creditcard_settings', false) ?: [];
-        $enabled = isset($creditCardSettings[self::MOLLIE_CREDITCARD_ICONS_ENABLER])
-            ? wc_string_to_bool($creditCardSettings[self::MOLLIE_CREDITCARD_ICONS_ENABLER])
-            : false;
-
-        if (!$enabled) {
-            return [];
-        }
-        $optionLexem = self::MOLLIE_CREDITCARD_ICONS;
-        $creditcardsAvailable = self::AVAILABLE_CREDITCARD_ICONS;
-        $svgFileName = self::SVG_FILE_EXTENSION;
-        $enabledCreditcards = [];
-
-        $creditcardSettings = get_option('mollie_wc_gateway_creditcard_settings');
-        foreach ($creditcardsAvailable as $card) {
-            if ($creditcardSettings[$optionLexem . $card] === 'yes') {
-                $enabledCreditcards[] = $card . $svgFileName;
-            }
-        }
-
-        return $enabledCreditcards;
-    }
-
-    /**
-     * Method to write the svg file with the composed svg
-     * Can be used to reset the file to empty
-     *
-     * @param string $content svg content to compose the image if empty will
-     *                        reset the file
-     *
-     * @return string the url to the written file
-     */
-    protected function writeToSvgFile($content = '')
-    {
-        $uploads = wp_upload_dir();
-        file_put_contents(
-            $uploads['basedir'] . '/compositeCards.svg',
-            $content
-        );
-        return $uploads['baseurl'] . '/compositeCards.svg';
-    }
-
-    /**
-     * @param array $enabledCreditCards Array of the selected cards to compose
-     *                                  the image with
-     *
-     * @return string Newly composed svg string
-     */
-    protected function buildSvgComposed($enabledCreditCards)
-    {
-        $assetsImagesPath
-            = '/app/public/wp-content/plugins/WooCommerce-Mollie/public/images/Creditcard_issuers/';
-        $cardWidth = self::CREDIT_CARD_ICON_WIDTH;
-        $cardsNumber = count($enabledCreditCards);
-        $cardsWidth = $cardWidth * $cardsNumber;
-        $cardPositionX = 0;
-        $actual
-            = "<svg width=\"{$cardsWidth}\" height=\"24\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">";
-        foreach ($enabledCreditCards as $creditCard) {
-            $actual .= $this->positionSvgOnX(
-                $cardPositionX,
-                file_get_contents(
-                    $assetsImagesPath . $creditCard
-                )
-            );
-            $cardPositionX += $cardWidth;
-        }
-        $actual .= "</svg>";
-
-        return $actual;
-    }
-
-    /**
-     * Method to add the x parameter to the svg string so that the icon can
-     * be positioned related to other icons.
-     *
-     * @param int    $xPosition coordinate to position icon on x axis
-     * @param string $svgString svg string to add position to
-     *
-     * @return string|string[] Modified svg string with the x position added
-     */
-    protected function positionSvgOnX($xPosition, $svgString)
-    {
-        $positionString = " x=\"{$xPosition}\"";
-        $positionAfterSvgWord = 4;
-
-        return substr_replace($svgString, $positionString, $positionAfterSvgWord, 0);
     }
 }
 

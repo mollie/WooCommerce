cb5893593328c7449b729b5d5e18433eea75dcd8
carmenmaymo <mayen22@gmail.com>
2020-09-11 10:53:30 +0200

    Add check to post var
    MOL-207

--- src/Mollie/WC/Payment/Order.php
+++ src/Mollie/WC/Payment/Order.php
@@ -193,12 +193,15 @@ class Mollie_WC_Payment_Order extends Mollie_WC_Payment_Object {
             $paymentRequestData['payment']['cardToken'] = $cardToken;
         }
 
-        $applePayToken = $_POST['token'];
-        $applePayToken = filter_var($applePayToken, FILTER_SANITIZE_STRING);
-        if($applePayToken && isset($paymentRequestData['payment'])){
-            $encodedApplePayToken = json_encode($applePayToken);
-            $paymentRequestData['payment']['applePayPaymentToken'] = $encodedApplePayToken;
+        if($_POST['token']){
+            $applePayToken = $_POST['token'];
+            $applePayToken = filter_var($applePayToken, FILTER_SANITIZE_STRING);
+            if($applePayToken && isset($paymentRequestData['payment'])){
+                $encodedApplePayToken = json_encode($applePayToken);
+                $paymentRequestData['payment']['applePayPaymentToken'] = $encodedApplePayToken;
+            }
         }
+
 
 
         return $paymentRequestData;

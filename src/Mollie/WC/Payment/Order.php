11bff1155d227539f8fad61a93190d5b05242941
Jeroen Groenveld <jeroen.groenveld@j20.nl>
2020-10-02 13:58:01 +0200

    Check POST data existence using isset to prevent: NOTICE Undefined index: token

--- src/Mollie/WC/Payment/Order.php
+++ src/Mollie/WC/Payment/Order.php
@@ -193,7 +193,7 @@ class Mollie_WC_Payment_Order extends Mollie_WC_Payment_Object {
             $paymentRequestData['payment']['cardToken'] = $cardToken;
         }
 
-        if($_POST['token']){
+        if(isset($_POST['token'])){
             $applePayToken = filter_input(INPUT_POST, 'token', FILTER_SANITIZE_STRING);
             if($applePayToken && isset($paymentRequestData['payment'])){
                 $encodedApplePayToken = json_encode($applePayToken);

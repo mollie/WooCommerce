008b5665829ccd5eec8e1f1f5f8482f8d59a42e8
carmenmaymo <mayen22@gmail.com>
2020-12-24 09:49:49 +0100

    Add notice on api keys missing

--- inc/settings/mollie_components_enabler.php
+++ inc/settings/mollie_components_enabler.php
@@ -6,7 +6,7 @@ return [
         'title' => __('Enable Mollie Components', 'mollie-payments-for-woocommerce'),
         'description' => sprintf(
             __(
-                'Use the Mollie Components for this Gateway.Read more about <a href="https://www.mollie.com/en/news/post/better-checkout-flows-with-mollie-components">%s</a> and how it improves your conversion',
+                'Use the Mollie Components for this Gateway. Read more about <a href="https://www.mollie.com/en/news/post/better-checkout-flows-with-mollie-components">%s</a> and how it improves your conversion.',
                 'mollie-payments-for-woocommerce'
             ),
             __('Mollie Components', 'mollie-payments-for-woocommerce')

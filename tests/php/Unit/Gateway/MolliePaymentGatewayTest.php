a364d322f86e7a71a92cf40bb2e6f9e41ef9a8da
carmenmaymo <mayen22@gmail.com>
2021-09-28 09:42:07 +0200

    add some test cases

---
+++ tests/php/Unit/Gateway/MolliePaymentGatewayTest.php
@@ -0,0 +1,97 @@
+<?php
+
+
+use Mollie\WooCommerce\Gateway\MolliePaymentGateway;
+use PHPUnit\Framework\TestCase;
+
+class MolliePaymentGatewayTest extends TestCase
+{
+
+	public function testInit_form_fields()
+	{
+	}
+
+	public function testThankyou_page()
+	{
+	}
+
+	public function testGetSelectedIssuer()
+	{
+	}
+
+	public function testDisplayInstructions()
+	{
+	}
+
+	public function testGetProcessPaymentRedirect()
+	{
+	}
+
+	public function testGet_icon()
+	{
+	}
+
+	public function testIs_available()
+	{
+	}
+
+	public function testValidate_multi_select_countries_field()
+	{
+	}
+
+	public function testProcess_admin_options()
+	{
+	}
+
+	public function testOnOrderReceivedTitle()
+	{
+	}
+
+	public function testInit_settings()
+	{
+	}
+
+	public function testAdmin_options()
+	{
+	}
+
+	public function testGetReturnRedirectUrlForOrder()
+	{
+	}
+
+	public function testGetMollieMethod()
+	{
+	}
+
+	public function testOnOrderReceivedText()
+	{
+	}
+
+	public function testGetIconUrl()
+	{
+	}
+
+	public function testPaymentConfirmationAfterCoupleOfDays()
+	{
+	}
+
+	public function testProcess_refund()
+	{
+	}
+
+	public function testProcess_payment()
+	{
+	}
+
+	public function testGet_transaction_url()
+	{
+	}
+
+	public function testPayment_fields()
+	{
+	}
+
+	public function testHandlePaidOrderWebhook()
+	{
+	}
+}

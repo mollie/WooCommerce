562d6210b736ff5ee800d68779f0cde58663b4c8
carmenmaymo <mayen22@gmail.com>
2022-02-10 08:54:13 +0100

    change version number for new release

--- src/Shared/SharedModule.php
+++ src/Shared/SharedModule.php
@@ -29,7 +29,7 @@ class SharedModule implements ServiceModule
             },
             'shared.plugin_version' => static function (): string {
                 //Get plugin version
-                return '7.0.0';
+                return '7.0.1';
             },
             'shared.plugin_title' => static function (): string {
                 //Get plugin version

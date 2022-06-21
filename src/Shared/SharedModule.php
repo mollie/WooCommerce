96be4084c7cc868481b6abf0a2c1b14b9a8e0548
carmenmaymo <mayen22@gmail.com>
2022-06-21 09:50:15 +0200

    MOL-857 Update version number

--- src/Shared/SharedModule.php
+++ src/Shared/SharedModule.php
@@ -29,7 +29,7 @@ class SharedModule implements ServiceModule
             },
             'shared.plugin_version' => static function (): string {
                 //Get plugin version
-                return '7.2.0-beta1';
+                return '7.2.0';
             },
             'shared.plugin_title' => static function (): string {
                 //Get plugin version

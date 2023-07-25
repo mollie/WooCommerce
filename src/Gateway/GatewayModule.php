d4ed703712c0eaf53edf9610f56531b0bdd4a90c
carmenmaymo <mayen22@gmail.com>
2023-07-25 10:49:15 +0200

    Fix cs

--- src/Gateway/GatewayModule.php
+++ src/Gateway/GatewayModule.php
@@ -95,7 +95,7 @@ class GatewayModule implements ServiceModule, ExecutableModule
                 }
                 return $availableMethods;
             },
-            'gateway.getPaymentMethodsAfterFeatureFlag' => static function (ContainerInterface $container):array {
+            'gateway.getPaymentMethodsAfterFeatureFlag' => static function (ContainerInterface $container): array {
                 $availablePaymentMethods = $container->get('gateway.listAllMethodsAvailable');
                 $klarnaOneFlag = apply_filters('inpsyde.feature-flags.mollie-woocommerce.klarna_one_enabled', getenv('MOL_KLARNA_ENABLED') === '1');
                 //remove other klarna, leave only klarna one

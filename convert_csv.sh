e58ff1f0f62e1627971c3bed7febe5c113837342
carmenmaymo <mayen22@gmail.com>
2023-09-26 12:38:58 +0200

    Update scripts to work with separated full files

--- convert_csv.sh
+++ convert_csv.sh
@@ -1,76 +1,77 @@
 #!/bin/bash
 
 cd languages || exit
+
 POT_FILE="en_GB.pot"
-PO_FILE="mollie-payments-for-woocommerce-nl_NL.po"
-TEMP_POT="temp_untranslated.pot"
-CSV_FILE="en_GB.csv"
+CSV_DIR="untranslated_csv"
+
+LANG_EXT=("-de_DE" "-de_DE_formal" "-es_ES" "-fr_FR" "-it_IT" "-nl_NL" "-nl_NL_formal" "-nl_BE" "-nl_BE_formal")
 
 if [[ ! -e $POT_FILE ]]; then
     echo "File $POT_FILE not found."
     exit 1
 fi
 
-if [[ ! -e $PO_FILE ]]; then
-    echo "File $POT_FILE not found."
-    exit 1
-fi
+mkdir -p $CSV_DIR  # Create the directory if it does not exist
 
-current_msgid=""
-current_msgctxt=""
-block=""
+# Loop through each language extension
+for lang in "${LANG_EXT[@]}"; do
+    PO_FILE="mollie-payments-for-woocommerce${lang}.po"
+    CSV_FILE="$CSV_DIR/mollie-payments-for-woocommerce${lang}.csv"
+    UNTRANSLATED_FILE="$CSV_DIR/mollie-payments-for-woocommerce${lang}_untranslated.csv"
 
-while IFS= read -r line; do
-    block="$block$line\n"
-    if [[ "$line" =~ ^msgid\ \"(.+)\"$ ]]; then
-        current_msgid="${BASH_REMATCH[1]}"
+    if [[ -e $PO_FILE ]]; then
+        msgmerge --update --no-wrap --backup=none "$PO_FILE" "$POT_FILE"  # Update PO file with POT file
+        echo "Updated $PO_FILE with new strings from $POT_FILE"
+    else
+        echo "File $PO_FILE not found. Skipping..."
+        continue
     fi
 
-    if [[ "$line" =~ ^msgctxt\ \"(.+)\"$ ]]; then
-        current_msgctxt="${BASH_REMATCH[1]}"
-    fi
+    echo 'location,msgid,msgstr,msgctxt' > "$CSV_FILE"
+    echo 'line,msgid' > "$UNTRANSLATED_FILE"
 
-    if [[ "$line" =~ ^msgstr ]]; then
-        if ! grep -Fq "msgid \"$current_msgid\"" "$PO_FILE" || ( [ -n "$current_msgctxt" ] && ! grep -Fq "msgctxt \"$current_msgctxt\"" "$PO_FILE" ); then
-            echo -e "$block" >> $TEMP_POT
+    # Initialize variables
+    location=""
+    msgid=""
+    msgstr=""
+    msgctxt=""
+    line_no=0
+
+    # Function to write to CSV
+    write_to_csv() {
+        echo "\"$location\",\"$msgid\",\"$msgstr\",\"$msgctxt\"" >> "$CSV_FILE"
+        if [[ -z "$msgstr" && -n "$msgid" ]]; then
+            echo "$line_no,\"$msgid\"" >> "$UNTRANSLATED_FILE"
         fi
-        current_msgid=""
-        current_msgctxt=""
-        block=""
-    fi
-done < "$POT_FILE"
-
-echo 'msgid,msgstr,msgctxt,location,-de_DE, -de_DE_formal, -es_ES, -fr_FR, -it_IT, -nl_NL, -nl_NL_formal, -nl_BE, -nl_BE_formal' > $CSV_FILE
-location=""
-msgid=""
-msgstr=""
-msgctxt=""
-
-while IFS= read -r line; do
-    if [[ "$line" =~ ^\#:\ (.+)$ ]]; then
-        location="${BASH_REMATCH[1]}"
-    fi
-
-    if [[ "$line" =~ ^msgid\ \"(.+)\"$ ]]; then
-        msgid="${BASH_REMATCH[1]}"
-    fi
-
-    if [[ "$line" =~ ^msgstr\ \"(.+)\"$ ]]; then
-        msgstr="${BASH_REMATCH[1]}"
-    fi
-
-    if [[ "$line" =~ ^msgctxt\ \"(.+)\"$ ]]; then
-        msgctxt="${BASH_REMATCH[1]}"
-    fi
-
-    if [[ "$line" =~ ^msgstr ]]; then
-        echo "\"$msgid\",\"$msgstr\",\"$msgctxt\",\"$location\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\" " >> $CSV_FILE
+        # Reset variables
         location=""
         msgid=""
         msgstr=""
         msgctxt=""
+    }
+
+    while IFS= read -r line || [[ -n "$line" ]]; do  # also handle the last line
+        ((line_no++))
+
+        if [[ "$line" =~ ^\#:\ (.+)$ ]]; then
+            location="${BASH_REMATCH[1]}"
+        elif [[ "$line" =~ ^msgid\ \"(.*)\"$ ]]; then
+            if [[ -n "$msgid" || -n "$msgstr" ]]; then
+                write_to_csv
+            fi
+            msgid="${BASH_REMATCH[1]}"
+        elif [[ "$line" =~ ^msgstr\ \"(.*)\"$ ]]; then
+            msgstr="${BASH_REMATCH[1]}"
+        elif [[ "$line" =~ ^msgctxt\ \"(.+)\"$ ]]; then
+            msgctxt="${BASH_REMATCH[1]}"
+        fi
+    done < "$PO_FILE"
+
+    # For the last msgid in the file
+    if [[ -n "$msgid" || -n "$msgstr" ]]; then
+        write_to_csv
     fi
 
-done < "$TEMP_POT"
-
-rm $TEMP_POT
+    echo "Created CSV $CSV_FILE and $UNTRANSLATED_FILE for $PO_FILE"
+done

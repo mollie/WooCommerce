edfada73b73a90bc3baeb0fdcd93f374ae696fa6
carmenmaymo <mayen22@gmail.com>
2023-11-06 10:27:14 +0100

    Extract testData in error handling test

--- tests/Playwright/tests/Error Handling/_error_handling.spec.js
+++ tests/Playwright/tests/Error Handling/_error_handling.spec.js
@@ -2,6 +2,7 @@ const { expect } = require('@playwright/test');
 const { test } = require('../Shared/base-test');
 const {getLogByName} = require("../Shared/wooUtils");
 const assert = require('assert');
+const {testData} = require("./testData");
 
 test.describe(' - Error Handling', () => {
     let log;
@@ -10,48 +11,9 @@ test.describe(' - Error Handling', () => {
         log = await getLogByName(searchString, __dirname);
     });
 
-    const testData = [
-        {
-            testId: "C419987",
-            mollieStatus: "Paid",
-            searchLine: "onWebhookPaid processing paid order via Mollie plugin fully completed"
-        },
-        {
-            testId: "C420052",
-            mollieStatus: "Authorized",
-            searchLine: "onWebhookAuthorized called for order",
-        },
-        {
-            testId: "C420052",
-            mollieStatus: "Open",
-            searchLine: "Customer returned to store, but payment still pending for order",
-        },
-        {
-            testId: "C419988",
-            mollieStatus: "Failed",
-            searchLine: "onWebhookFailed called for order",
-        },
-        {
-            testId: "C420050",
-            mollieStatus: "Canceled",
-            searchLine: "Pending payment",
-        },
-        {
-            testId: "C420051",
-            mollieStatus: "Expired",
-            searchLine: "Pending payment",
-        },
-        {
-            testId: "C420054",
-            mollieStatus: "Pending",
-            wooStatus: "Pending payment",
-        },
-    ];
-
     testData.forEach(({ testId, mollieStatus, searchLine }) => {
         test(`[${testId}] Validate that "${mollieStatus} transaction is logged"`, async ({ page, products, context }) => {
             const pattern = new RegExp(searchLine, 'g');
-            console.log(pattern)
             const containsPattern = pattern.test(log);
             assert.ok(containsPattern, 'The file content does not contain the desired string');
         });

a6a9a1ae9ad5c55e05871b85fe0b8ea89f61ecee
carmenmaymo <mayen22@gmail.com>
2024-04-29 12:07:55 +0200

    Change apple validation error string PIWOO-227

--- src/Buttons/ApplePayButton/ApplePayDirectHandler.php
+++ src/Buttons/ApplePayButton/ApplePayDirectHandler.php
@@ -53,14 +53,12 @@ class ApplePayDirectHandler
         }
 
         if (!$this->merchantValidated()) {
-            /* translators: Placeholder 1: Opening strong tag. Placeholder 2: Closing strong tag. Placeholder 3: Opening link tag to documentation. Placeholder 4: Closing link tag.*/
+            /* translators: Placeholder 1: Opening link tag to documentation. Placeholder 2: Closing link tag.*/
             $message = sprintf(
-                esc_html__(
-                    '%1$sApple Pay Validation Error%2$s Check %3$sApple Server requirements page%4$s to fix it in order to make the Apple Pay button work',
-                    'mollie-payments-for-woocommerce'
-                ),
-                '<strong>',
-                '</strong>',
+                    esc_html__(
+                            'Apple Pay Validation Error: Please review the %1$sApple Server requirements%2$s. If everything appears correct, click the Apple Pay button to retry validation.',
+                            'mollie-payments-for-woocommerce'
+                    ),
                 '<a href="https://developer.apple.com/documentation/apple_pay_on_the_web/setting_up_your_server">',
                 '</a>'
             );

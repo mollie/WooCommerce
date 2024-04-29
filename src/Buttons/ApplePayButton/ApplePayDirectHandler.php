02ffdac097c813c3f9545596eaf9a15a3464354d
carmenmaymo <mayen22@gmail.com>
2024-04-29 12:14:20 +0200

    Fix phpcs PIWOO-227

--- src/Buttons/ApplePayButton/ApplePayDirectHandler.php
+++ src/Buttons/ApplePayButton/ApplePayDirectHandler.php
@@ -55,10 +55,10 @@ class ApplePayDirectHandler
         if (!$this->merchantValidated()) {
             /* translators: Placeholder 1: Opening link tag to documentation. Placeholder 2: Closing link tag.*/
             $message = sprintf(
-                    esc_html__(
-                            'Apple Pay Validation Error: Please review the %1$sApple Server requirements%2$s. If everything appears correct, click the Apple Pay button to retry validation.',
-                            'mollie-payments-for-woocommerce'
-                    ),
+                esc_html__(
+                    'Apple Pay Validation Error: Please review the %1$sApple Server requirements%2$s. If everything appears correct, click the Apple Pay button to retry validation.',
+                    'mollie-payments-for-woocommerce'
+                ),
                 '<a href="https://developer.apple.com/documentation/apple_pay_on_the_web/setting_up_your_server">',
                 '</a>'
             );

c3f3f69eac96387a31ca28d1a30a0309f2e5458d
carmenmaymo <mayen22@gmail.com>
2024-09-17 10:43:35 +0200

    Add scoper and remove mozart files

---
+++ scoper.inc.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+// scoper.inc.php
+
+use Isolated\Symfony\Component\Finder\Finder;
+
+$wp_classes = json_decode(
+    file_get_contents(
+        __DIR__.'/vendor/sniccowp/php-scoper-wordpress-excludes/generated/exclude-wordpress-classes.json'
+    ),
+    true
+);
+$wp_constants = json_decode(
+    file_get_contents(
+        __DIR__.'/vendor/sniccowp/php-scoper-wordpress-excludes/generated/exclude-wordpress-constants.json'
+    ),
+    true
+);
+$wp_functions = json_decode(
+    file_get_contents(
+        __DIR__.'/vendor/sniccowp/php-scoper-wordpress-excludes/generated/exclude-wordpress-functions.json'
+    ),
+    true
+);
+
+$finders = [
+    Finder::create()
+        ->files()
+        ->ignoreVCS(true)
+        ->ignoreDotFiles(false) # We need to keep .distignore around
+        ->exclude([
+                      '.github',
+                      '.ddev',
+                      '.idea',
+                      'modules.local',
+                      'tests',
+                  ])
+        ->in('.'),
+];
+
+return [
+    'prefix' => 'Syde\\Vendor', // string|null
+    'finders' => $finders,      // list<Finder>
+    'patchers' => [], // list<callable(string $filePath, string $prefix, string $contents): string>
+    'exclude-files' => [
+        'vendor/symfony/polyfill-php80/Resources/stubs/Stringable.php'
+    ], // list<string>
+    'exclude-namespaces' => [
+        'Composer',
+        'Automattic',
+        '^WooCommerce',
+        'Inpsyde\Assets',
+    ], // list<string|regex>
+    'exclude-constants' => array_merge($wp_constants, [
+        'WC_VERSION',
+    ]), // list<string|regex>
+    'exclude-classes' => array_merge($wp_classes, [
+        'WooCommerce',
+        '/^WC_/',
+    ]),     // list<string|regex>
+    'exclude-functions' => array_merge($wp_functions, [
+        '/^wc/',
+    ]), // list<string|regex>
+
+    'expose-global-constants' => false,   // bool
+    'expose-global-classes' => false,     // bool
+    'expose-global-functions' => false,   // bool
+
+    'expose-namespaces' => [], // list<string|regex>
+    'expose-constants' => [],  // list<string|regex>
+    'expose-classes' => [],    // list<string|regex>
+    'expose-functions' => [],  // list<string|regex>
+];

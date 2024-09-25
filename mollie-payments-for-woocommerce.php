d725bc8ca72c52b9c28d8259642560ba7fa03e42
carmenmaymo <mayen22@gmail.com>
2024-09-25 09:49:28 +0200

    Update plugin version

--- mollie-payments-for-woocommerce.php
+++ mollie-payments-for-woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name: Mollie Payments for WooCommerce
  * Plugin URI: https://www.mollie.com
  * Description: Accept payments in WooCommerce with the official Mollie plugin
- * Version: 7.8.0
+ * Version: 7.8.1-beta
  * Author: Mollie
  * Author URI: https://www.mollie.com
  * Requires at least: 5.0
@@ -12,7 +12,7 @@
  * Domain Path: /languages
  * License: GPLv2 or later
  * WC requires at least: 3.9
- * WC tested up to: 9.1
+ * WC tested up to: 9.3
  * Requires PHP: 7.2
  * Requires Plugins: woocommerce
  */

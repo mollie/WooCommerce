536044fcc00bf3a4683069a4c88ce436b9c26503
carmenmaymo <mayen22@gmail.com>
2024-12-04 14:23:40 +0100

    add 471 fix

--- resources/js/mollieBlockIndex.js
+++ resources/js/mollieBlockIndex.js
@@ -2,77 +2,71 @@ import molliePaymentMethod from './blocks/molliePaymentMethod'
 import ApplePayButtonComponent from './blocks/ApplePayButtonComponent'
 
 (
-    function ({ mollieBlockData, wc, _, jQuery}) {
+    function ({mollieBlockData, wc, _, jQuery}) {
         if (_.isEmpty(mollieBlockData)) {
             return;
         }
+        const {registerPaymentMethod} = wc.wcBlocksRegistry;
+        const {checkoutData, defaultFields} = wc.wcSettings.allSettings;
+        let billing_address, shipping_address;
+        if (checkoutData) {
+            ({billing_address, shipping_address} = checkoutData);
+        } else {
+            billing_address = {};
+            shipping_address = {};
+        }
+        const {ajaxUrl, filters, gatewayData, availableGateways} = mollieBlockData.gatewayData;
+        const {useEffect} = wp.element;
+        const isAppleSession = typeof window.ApplePaySession === "function"
+        const isBlockEditor = !!wp?.blockEditor;
 
-        window.onload = (event) => {
-            const { registerPaymentMethod } = wc.wcBlocksRegistry;
-            const { checkoutData, defaultFields } = wc.wcSettings.allSettings;
-            let billing_address, shipping_address;
+        function getCompanyField() {
+            let shippingCompany = shipping_address.company ?? false;
+            let billingCompany = billing_address.company ?? false;
+            return shippingCompany ? shippingCompany : billingCompany;
+        }
 
-            if (checkoutData) {
-                ({ billing_address, shipping_address } = checkoutData);
-            } else {
-                billing_address = {};
-                shipping_address = {};
+        function getPhoneField() {
+            const phoneFieldDataset = document.querySelector('[data-show-phone-field]');
+            if (!phoneFieldDataset) {
+                return true;
             }
-            const { ajaxUrl, filters, gatewayData, availableGateways } = mollieBlockData.gatewayData;
-            const {useEffect} = wp.element;
-            const isAppleSession = typeof window.ApplePaySession === "function"
-            const isBlockEditor = !!wp?.blockEditor;
+            return phoneFieldDataset.dataset.showPhoneField !== "false"
+        }
 
-            function getCompanyField() {
-                let shippingCompany = shipping_address.company ?? false;
-                let billingCompany = billing_address.company ?? false;
-                return shippingCompany ? shippingCompany : billingCompany;
-            }
-
-            function getPhoneField()
-            {
-                const phoneFieldDataset = document.querySelector('[data-show-phone-field]');
-                if (!phoneFieldDataset) {
-                    return true;
-                }
-                return phoneFieldDataset.dataset.showPhoneField !== "false"
-            }
-
-            const isCompanyFieldVisible = getCompanyField();
-            const companyNameString = defaultFields.company.label
-            const isPhoneFieldVisible = getPhoneField();
-            const phoneString = defaultFields.phone.label
-            let requiredFields = {
-                'companyNameString': companyNameString,
-                'phoneString': phoneString,
-            }
-            gatewayData.forEach(item => {
-                let register = () => registerPaymentMethod(molliePaymentMethod(useEffect, ajaxUrl, filters, gatewayData, availableGateways, item, jQuery, requiredFields, isCompanyFieldVisible, isPhoneFieldVisible));
-                if (item.name === 'mollie_wc_gateway_applepay'  && !isBlockEditor) {
-                     const {isExpressEnabled} = item;
-                    if ((isAppleSession && window.ApplePaySession.canMakePayments())) {
-                        register();
-                        if (isExpressEnabled !== true) {
-                            return;
-                        }
-                        const {registerExpressPaymentMethod} = wc.wcBlocksRegistry;
-                        registerExpressPaymentMethod({
-                            name: 'mollie_wc_gateway_applepay_express',
-                            content: <ApplePayButtonComponent />,
-                            edit: <ApplePayButtonComponent />,
-                            ariaLabel: 'Apple Pay',
-                            canMakePayment: () => true,
-                            paymentMethodId: 'mollie_wc_gateway_applepay',
-                            supports: {
-                                features: ['products'],
-                            },
-                        })
+        const isCompanyFieldVisible = getCompanyField();
+        const companyNameString = defaultFields.company.label
+        const isPhoneFieldVisible = getPhoneField();
+        const phoneString = defaultFields.phone.label
+        let requiredFields = {
+            'companyNameString': companyNameString,
+            'phoneString': phoneString,
+        }
+        gatewayData.forEach(item => {
+            let register = () => registerPaymentMethod(molliePaymentMethod(useEffect, ajaxUrl, filters, gatewayData, availableGateways, item, jQuery, requiredFields, isCompanyFieldVisible, isPhoneFieldVisible));
+            if (item.name === 'mollie_wc_gateway_applepay' && !isBlockEditor) {
+                const {isExpressEnabled} = item;
+                if ((isAppleSession && window.ApplePaySession.canMakePayments())) {
+                    register();
+                    if (isExpressEnabled !== true) {
+                        return;
                     }
-                    return;
+                    const {registerExpressPaymentMethod} = wc.wcBlocksRegistry;
+                    registerExpressPaymentMethod({
+                        name: 'mollie_wc_gateway_applepay_express',
+                        content: <ApplePayButtonComponent/>,
+                        edit: <ApplePayButtonComponent/>,
+                        ariaLabel: 'Apple Pay',
+                        canMakePayment: () => true,
+                        paymentMethodId: 'mollie_wc_gateway_applepay',
+                        supports: {
+                            features: ['products'],
+                        },
+                    })
                 }
-                register();
-            });
-        };
-
+                return;
+            }
+            register();
+        });
     }
 )(window, wc)

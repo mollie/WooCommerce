9a75ec0dce23616d612f2190c67dbfb7345d1e06
Arnout Pullen <arnoutpullen@hotmail.com>
2025-01-06 13:56:17 +0100

    Only show billie billing company field when missing from checkout

--- src/Payment/MollieObject.php
+++ src/Payment/MollieObject.php
@@ -1109,7 +1109,7 @@ class MollieObject
         $isBillieMethodId = $gateway->id === 'mollie_wc_gateway_billie';
         if ($isBillieMethodId) {
             //phpcs:ignore WordPress.Security.NonceVerification, WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
-            $fieldPosted = wc_clean(wp_unslash($_POST["billing_company"] ?? ''));
+            $fieldPosted = wc_clean(wp_unslash($_POST["billing_company_billie"] ?? $_POST["billing_company"] ?? ''));
             if ($fieldPosted === '' || !is_string($fieldPosted)) {
                 return null;
             }

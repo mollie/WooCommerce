0bbc2b41aa0ddbf3b6090aecb48d4c7d8715ba21
Daniel Hüsken <mail@danielhuesken.de>
2025-01-28 10:28:48 +0100

    add feature flag for MBWay

--- src/Gateway/GatewayModule.php
+++ src/Gateway/GatewayModule.php
@@ -130,6 +130,12 @@ class GatewayModule implements ServiceModule, ExecutableModule
                         return $method['id'] !== Constants::SWISH;
                     });
                 }
+                $swishFlag = (bool) apply_filters('inpsyde.feature-flags.mollie-woocommerce.mbway_enabled', false);
+                if (!$swishFlag) {
+                    $availablePaymentMethods = array_filter($availablePaymentMethods, static function ($method) {
+                        return $method['id'] !== 'mbway';
+                    });
+                }
                 return $availablePaymentMethods;
             },
             'gateway.isSDDGatewayEnabled' => static function (ContainerInterface $container): bool {

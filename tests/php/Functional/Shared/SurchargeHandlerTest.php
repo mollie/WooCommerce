367cd5b17362a7daf521fb4e63a92125ccbb8fd7
Daniel Hüsken <mail@danielhuesken.de>
2025-04-17 16:03:25 +0200

    Fix Test

--- tests/php/Functional/Shared/SurchargeHandlerTest.php
+++ tests/php/Functional/Shared/SurchargeHandlerTest.php
@@ -27,47 +27,6 @@ class SurchargeHandlerTest extends TestCase
         parent::__construct($name, $data, $dataName);
         $this->helperMocks = new HelperMocks();
     }
-    /**
-     * with all surcharge types
-     * GIVEN I'm in the checkout and the surcharge is set
-     * WHEN the cart is above the fee minimum
-     * THEN the cart will call add_fee with the proper fee name and amount
-     *
-     * @test
-     */
-    public function addsSurchargeFeesInCheckout(){
-        $cart = $this->cartMock();
-        $paymentSurcharge = Surcharge::FIXED_FEE;
-        $fixedFee = 10.00;
-        $percentage = 0;
-        $feeLimit = 1;
-        $expectedLabel = 'Gateway Fee';
-        $expectedAmount = 10.00;
-        expect('get_option')->andReturn(
-            'Gateway Fee',
-            $this->helperMocks->paymentMethodSettings(
-                [
-                    'payment_surcharge' => $paymentSurcharge,
-                    'surcharge_limit' => $feeLimit,
-                    'fixed_fee' => $fixedFee,
-                    'percentage' => $percentage,
-                ]
-            )
-        );
-        $testee = $this->buildTesteeMock(
-            GatewaySurchargeHandler::class,
-            [new Surcharge()],
-            ['canProcessOrder', 'canProcessGateway', 'orderRemoveFee', 'orderAddFee']
-        )->getMock();
-        $testee->initializeGatewayFeeLabel();
-        expect('mollieWooCommerceIsCheckoutContext')->andReturn(true);
-        expect('wc_tax_enabled')->andReturn(false);
-        expect('WC')->andReturn($this->wooCommerce());
-        expect('is_admin')->andReturn(false);
-
-        $cart->expects(self::once())->method('add_fee')->with($expectedLabel, $expectedAmount, true, 'standard');
-        $testee->add_engraving_fees($cart);
-    }
 
     /**
      *
@@ -139,22 +98,6 @@ class SurchargeHandlerTest extends TestCase
                 'get_subtotal_tax' => '2.50',
             ]
         );
-    }
-    /**
-     *
-     * @return PHPUnit_Framework_MockObject_MockObject
-     * @throws PHPUnit_Framework_Exception
-     */
-    public function wooCommerce() {
-        $wc = $this->createConfiguredMock(
-            'WooCommerce',
-            [
-
-            ]
-        );
-        $wc->session = new \WC_Session();
-        $wc->session->chosen_payment_method = 'mollie_wc_gateway_ideal';
-        return $wc;
     }
 
     /**

04fe3b08a99b9082c03c4ae0aea62914f9e60dfa
Daniel Hüsken <mail@danielhuesken.de>
2025-05-13 10:02:27 +0200

    Remove order Mandatory for all payment methods because it is not necessary anymore. Added only code for deprecated Klarna methods to hide them on the payments API.

--- src/PaymentMethods/Klarnasliceit.php
+++ src/PaymentMethods/Klarnasliceit.php
@@ -21,7 +21,6 @@ class Klarnasliceit extends AbstractPaymentMethod implements PaymentMethodI
             ],
             'filtersOnBuild' => false,
             'confirmationDelayed' => false,
-            'orderMandatory' => true,
             'docs' => 'https://www.mollie.com/gb/payments/klarna',
         ];
     }

fcdcaf8be4c50f5c37c9a6d70c3547cd80ce3501
Daniel Hüsken <mail@danielhuesken.de>
2025-05-16 13:56:05 +0200

    fix that Vouchers GW is not displayed in checkout

--- src/BlockService/CheckoutBlockService.php
+++ src/BlockService/CheckoutBlockService.php
@@ -94,8 +94,7 @@ class CheckoutBlockService
             if ($key !== 'mollie_wc_gateway_voucher') {
                 continue;
             }
-            $shouldRemoveVoucher = $this->voucherDisabler->shouldRemoveVoucher();
-            if ($shouldRemoveVoucher) {
+             if (!$this->voucherDisabler->haveCartProductsCategories()) {
                 unset($availableGateways[$key]);
             }
         }

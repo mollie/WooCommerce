dad29fb2460c7ef05a0970298d8e8cb5cf3864de
Daniel Hüsken <mail@danielhuesken.de>
2025-09-09 10:51:16 +0200

    set rest api default to on

--- src/Payment/Request/Middleware/UrlMiddleware.php
+++ src/Payment/Request/Middleware/UrlMiddleware.php
@@ -101,7 +101,7 @@ class UrlMiddleware implements RequestMiddlewareInterface
     public function getWebhookUrl(WC_Order $order, string $gatewayId): string
     {
         $webhookUrl = get_rest_url(null, RestApi::ROUTE_NAMESPACE . '/' . RestApi::WEBHOOK_ROUTE);
-        if (! $webhookUrl ||  ! wc_is_valid_url($webhookUrl) || apply_filters('mollie_wc_gateway_disable_rest_webhook', true)) {
+        if (! $webhookUrl ||  ! wc_is_valid_url($webhookUrl) || apply_filters('mollie_wc_gateway_disable_rest_webhook', false)) {
             $webhookUrl = WC()->api_request_url($gatewayId);
             $webhookUrl = untrailingslashit($webhookUrl);
             $webhookUrl = $this->asciiDomainName($webhookUrl);

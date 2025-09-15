af57f2043ba3768437fbba2c4c54540a82eda098
carmenmaymo <mayen22@gmail.com>
2025-09-15 14:39:41 +0200

    Improve CSS of phone field

--- resources/js/src/checkout/blocks/components/paymentFields/PhoneField.js
+++ resources/js/src/checkout/blocks/components/paymentFields/PhoneField.js
@@ -1,9 +1,9 @@
 export const PhoneField = ( { id, label, value, onChange, placeholder } ) => {
 	const handleChange = ( e ) => onChange( e.target.value );
-	const className = `wc-block-components-text-input wc-block-components-address-form__${ id }`;
+	const className = `wc-block-components-address-form__${ id }`;
 
 	return (
-		<div className="custom-input">
+		<div className="custom-input wc-block-components-text-input wc-block-components-address-form__phone is-active">
 			<label htmlFor={ id }>{ label }</label>
 			<input
 				type="tel"

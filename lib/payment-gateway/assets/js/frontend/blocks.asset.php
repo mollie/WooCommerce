b78bd6956600149ae9ce3236e1ce84b42520afca
carmenmaymo <mayen22@gmail.com>
2025-10-07 17:46:21 +0200

    Update library for express methods

--- lib/payment-gateway/assets/js/frontend/blocks.asset.php
+++ lib/payment-gateway/assets/js/frontend/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-hooks', 'wp-html-entities', 'wp-i18n'), 'version' => '0979f440305a0a9ee153');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-hooks', 'wp-html-entities', 'wp-i18n'), 'version' => '938cb742b4f17d2a16b7');

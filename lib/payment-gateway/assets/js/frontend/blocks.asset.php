7688ea60d4bfee72c6812ba745bd798d9d412e6d
carmenmaymo <mayen22@gmail.com>
2025-10-09 10:11:58 +0200

    Fix error in shop page Insydegateways are not there

--- lib/payment-gateway/assets/js/frontend/blocks.asset.php
+++ lib/payment-gateway/assets/js/frontend/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-hooks', 'wp-html-entities', 'wp-i18n'), 'version' => '938cb742b4f17d2a16b7');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-hooks', 'wp-html-entities', 'wp-i18n'), 'version' => '32c193b9221d2e84ad48');

54c8922fa69337d3c00bebd4418b30883a7338b8
carmenmaymo <mayen22@gmail.com>
2025-10-16 12:43:52 +0200

    Fix generic gateway icons failing on multistep checkout

--- lib/payment-gateway/assets/js/frontend/blocks.asset.php
+++ lib/payment-gateway/assets/js/frontend/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-hooks', 'wp-html-entities', 'wp-i18n'), 'version' => '32c193b9221d2e84ad48');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-hooks', 'wp-html-entities', 'wp-i18n'), 'version' => '0eebec4e10f9264415a2');

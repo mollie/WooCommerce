7b023e76a5f44a6d20686e590ad5ad16fddb0e4e
carmenmaymo <mayen22@gmail.com>
2025-10-20 12:41:28 +0200

    Remove icon log in gateway library

--- lib/payment-gateway/assets/js/frontend/blocks.asset.php
+++ lib/payment-gateway/assets/js/frontend/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-hooks', 'wp-html-entities', 'wp-i18n'), 'version' => '970a217f1824c0b27c48');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-hooks', 'wp-html-entities', 'wp-i18n'), 'version' => '437958257fa4f3ac9239');

efe9fdfe3c72a459a8a795ac694b7246a66cd34b
syde-mollie-deploybot <mollie-deploybot@syde.com>
2026-03-23 08:02:36 +0000

    [BOT] Add build artifact from dev/build-and-distribute -> build-and-distribute.

--- mollie-payments-for-woocommerce.php
+++ mollie-payments-for-woocommerce.php
@@ -4,7 +4,7 @@
  * Plugin Name: Mollie Payments for WooCommerce
  * Plugin URI: https://www.mollie.com
  * Description: Accept payments in WooCommerce with the official Mollie plugin
- * Version: 8.1.4+build-and-distribute.0b087e5
+ * Version: 8.1.4+build-and-distribute.a7fcc2e
  * Author: Mollie
  * Author URI: https://www.mollie.com
  * Requires at least: 5.0

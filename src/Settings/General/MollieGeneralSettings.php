fb2f02b59e895e55f9820c45f26c22434620c9cb
Moritz Meißelbach <m.meisselbach@syde.com>
2026-03-24 12:42:32 +0100

    Add Orders API logic to gateway form fields and settings handling

--- src/Settings/General/MollieGeneralSettings.php
+++ src/Settings/General/MollieGeneralSettings.php
@@ -2,7 +2,6 @@
 
 namespace Mollie\WooCommerce\Settings\General;
 
-use Mollie\WooCommerce\Gateway\MolliePaymentGatewayHandler;
 use Mollie\WooCommerce\Gateway\Surcharge;
 use Mollie\WooCommerce\Shared\SharedDataDictionary;
 
@@ -11,7 +10,8 @@ class MollieGeneralSettings
     public function gatewayFormFields(
         $defaultTitle,
         $defaultDescription,
-        $paymentConfirmation
+        $paymentConfirmation,
+        bool $isOrderApi = false
     ) {
 
         $formFields = [
@@ -283,9 +283,18 @@ class MollieGeneralSettings
         $formFields['hide_order_lines'] = [
             'title' => __('Hide order lines', 'mollie-payments-for-woocommerce'),
             'label' => __('Do not send order lines to Mollie', 'mollie-payments-for-woocommerce'),
-            'description' => __('Enable this option to prevent order line details from being sent to Mollie. This may be useful for certain payment methods or compliance requirements.', 'mollie-payments-for-woocommerce'),
+            'description' => $isOrderApi
+                ? __(
+                    'Not available when using the Orders API. Order lines are always sent for Orders API payments.',
+                    'mollie-payments-for-woocommerce'
+                )
+                : __(
+                    'Enable this option to prevent order line details from being sent to Mollie. This may be useful for certain payment methods or compliance requirements.',
+                    'mollie-payments-for-woocommerce'
+                ),
             'type' => 'checkbox',
             'default' => 'no',
+            'custom_attributes' => $isOrderApi ? ['disabled' => 'disabled'] : [],
         ];
 
         $formFields['activate_expiry_days_setting'] = [

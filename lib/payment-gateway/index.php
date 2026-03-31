620f2f20c53ac86769381c4982181ed074455ba7
syde-mollie-deploybot <mollie-deploybot@syde.com>
2026-03-31 18:20:02 +0000

    [BOT] Add build artifact from dev/PIWOO-714-introduce-phpstan-at-level-5 -> PIWOO-714-introduce-phpstan-at-level-5.

--- lib/payment-gateway/index.php
+++ lib/payment-gateway/index.php
@@ -7,8 +7,8 @@ namespace Mollie;
  * Plugin Name: ddev-wordpress-plugin-example
  * Plugin URI:  https://inpsyde.com
  * Description: {DESCRIPTION}
- * Version: 8.1.4+piwoo-714-introduce-phpstan-at-level-5.c40ff53
- * SHA: c40ff53725352d6157664abd53fa1189239331b7
+ * Version: 8.1.4+piwoo-714-introduce-phpstan-at-level-5.270630b
+ * SHA: 270630bcd1abbb5b24b7185e60704505651e3a80
  * Requires at least: 5.8
  * Requires PHP: 7.2
  * WC requires at least: 4.3

e8378b6ad5741e4bdca1fd862f9de763e934f2d8
Moritz Meißelbach <m.meisselbach@syde.com>
2026-04-01 19:10:05 +0200

    Remove unused plugin exports from QA resources index

--- tests/qa/resources/index.ts
+++ tests/qa/resources/index.ts
@@ -18,10 +18,3 @@ export * from './products';
 export * from './woocommerce-config';
 export * from './types';
 
-export { default as molliePlugin } from './mollie-plugin';
-export { default as disableNoncePlugin } from './disable-nonce-plugin';
-export { default as subscriptionsPlugin } from './woocommerce-subscriptions-plugin';
-export { default as disableWcSetupWizard } from './disable-wc-setup-wizard-plugin.json';
-export { default as disableGutenbergWelcomeGuide } from './disable-gutenberg-welcome-guide-plugin.json';
-export { default as featureFlagsPlugin } from './feature-flags-plugin';
-export { default as enableBizumPlugin } from './enable-bizum-plugin';

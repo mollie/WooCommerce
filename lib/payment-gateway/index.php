22f3cee2540b396691c3b33027726bef2fa6ea89
syde-mollie-deploybot <mollie-deploybot@syde.com>
2026-04-07 10:07:52 +0000

    [BOT] Add build artifact from dev/develop -> develop.

--- lib/payment-gateway/index.php
+++ lib/payment-gateway/index.php
@@ -7,8 +7,8 @@ namespace Mollie;
  * Plugin Name: ddev-wordpress-plugin-example
  * Plugin URI:  https://inpsyde.com
  * Description: {DESCRIPTION}
- * Version: 8.1.4+develop.93d6835
- * SHA: 93d683597804f98942299ee25cf8453ecd1e9539
+ * Version: 8.1.4+develop.efd78ac
+ * SHA: efd78ac6b06a248a8e4e0cfb9f9a48f822851505
  * Requires at least: 5.8
  * Requires PHP: 7.2
  * WC requires at least: 4.3

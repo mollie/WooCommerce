5d71721ba38310ac323d9e2618bb6989e4b4d651
Daniel Hüsken <mail@danielhuesken.de>
2026-04-07 10:01:53 +0200

    Fix fatal error by adding null check before minimal billing address validation

--- src/Payment/Request/Middleware/AddressMiddleware.php
+++ src/Payment/Request/Middleware/AddressMiddleware.php
@@ -49,8 +49,8 @@ class AddressMiddleware implements RequestMiddlewareInterface
         ) {
             $requestData['billingAddress'] = $billingAddress;
         }
-        //set billingAddress email or phone when no billing address is set for payment API
-        if ($this->shouldAddMinimalBillingAddress($requestData, $context, $billingAddress)) {
+        //set billingAddress email or phone when no billing address is set for payment API and information available
+        if ($billingAddress instanceof stdClass && $this->shouldAddMinimalBillingAddress($requestData, $context, $billingAddress)) {
             $requestData['billingAddress'] = $this->createMinimalBillingAddress($billingAddress);
         }
         // Only add shippingAddress if all required fields are set

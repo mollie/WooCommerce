b23cbe26c07e85ac17cd18c1987762d2b054fe20
Misha Utkin <m.utkin@inpsyde.com>
2026-04-09 11:18:24 +0200

    Refactor mollie.setup as separate setup blocks

--- tests/qa/tests/_setup/mollie.setup.ts
+++ tests/qa/tests/_setup/mollie.setup.ts
@@ -5,83 +5,35 @@ import { test as setup } from '../../utils';
 import {
 	taxSettings,
 	mollieApiKeys,
-	MollieSettings,
-	ShopConfig,
 	shopSettings,
 } from '../../resources';
 
-type EnvConfig = {
-	title: string;
-	store?: ShopConfig;
-	mollie?: {
-		cleanDb?: boolean;
-		apiKeys?: MollieSettings.ApiKeys;
-		apiMethod?: MollieSettings.ApiMethod;
-	};
-};
+// --- Checkout layout ---
 
-const configureEnv = ( data: EnvConfig ) => {
-	const { title, store, mollie } = data;
-
-	if ( store ) {
-		setup( `${ title } Setup store settings`, async ( { utils } ) => {
-			await utils.configureStore( store );
-		} );
-	}
-
-	if ( mollie ) {
-		const { cleanDb, apiKeys, apiMethod } = mollie;
-
-		setup( `${ title } Install/activate Mollie`, async ( { utils } ) => {
-			await utils.installAndActivateMollie();
-		} );
-
-		if ( cleanDb ) {
-			setup( `${ title } Clean Mollie DB`, async ( { mollieApi } ) => {
-				await mollieApi.setMollieApiKeys( mollieApiKeys.default );
-				await mollieApi.cleanMollieDb();
-			} );
-		}
-
-		if ( apiKeys ) {
-			setup( `${ title } Set API keys`, async ( { mollieApi } ) => {
-				await mollieApi.setMollieApiKeys( apiKeys );
-			} );
-		}
-
-		if ( apiMethod ) {
-			setup( `${ title } Set API method`, async ( { mollieApi } ) => {
-				await mollieApi.setAdvancedSettings( { apiMethod } );
-			} );
-		}
-	}
-};
-
-configureEnv( {
-	title: 'setup:checkout:block;',
-	store: { enableClassicPages: false },
+setup( 'setup:checkout:block;', async ( { utils } ) => {
+	await utils.configureStore( { enableClassicPages: false } );
 } );
 
-configureEnv( {
-	title: 'setup:checkout:classic;',
-	store: { enableClassicPages: true },
+setup( 'setup:checkout:classic;', async ( { utils } ) => {
+	await utils.configureStore( { enableClassicPages: true } );
 } );
 
-configureEnv( {
-	title: 'setup:tax:inc;',
-	store: { taxes: taxSettings.including },
+// --- Tax ---
+
+setup( 'setup:tax:inc;', async ( { utils } ) => {
+	await utils.configureStore( { taxes: taxSettings.including } );
 } );
 
-configureEnv( {
-	title: 'setup:tax:exc;',
-	store: { taxes: taxSettings.excluding },
+setup( 'setup:tax:exc;', async ( { utils } ) => {
+	await utils.configureStore( { taxes: taxSettings.excluding } );
 } );
 
-configureEnv( {
-	title: 'setup:mollie:germany;',
-	store: shopSettings.germany.general,
-	mollie: {
-		cleanDb: true,
-		apiKeys: mollieApiKeys.default,
-	},
+// --- Mollie Germany ---
+
+setup( 'setup:mollie:germany;', async ( { utils, mollieApi } ) => {
+	await utils.configureStore( shopSettings.germany.general );
+	await utils.installAndActivateMollie();
+	await mollieApi.setMollieApiKeys( mollieApiKeys.default );
+	await mollieApi.cleanMollieDb();
+	await mollieApi.setMollieApiKeys( mollieApiKeys.default );
 } );

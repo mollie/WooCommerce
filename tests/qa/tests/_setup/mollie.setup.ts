ac42dddd377d8fae93739f23743da23b15d9f20c
Misha Utkin <m.utkin@inpsyde.com>
2026-04-13 09:17:24 +0200

    Move env related scripts from mollie.setup to  env.setup

--- tests/qa/tests/_setup/mollie.setup.ts
+++ tests/qa/tests/_setup/mollie.setup.ts
@@ -1,53 +1,27 @@
 /**
  * Internal dependencies
  */
-import { createStorageStates, resetEnvironment, test as setup, setupWooCommerce } from '../../utils';
-import {
-	taxSettings,
-	mollieApiKeys,
-	shopSettings,
-} from '../../resources';
-
-// --- Reset env ---
-
-setup.describe( 'env:reset;', async () => {
-	setup( 'Setup: Reset Environment', async () => {
-		await resetEnvironment();
-	} );
-
-	setup( 'Setup: Create storage state', async () => {
-		await createStorageStates();
-	} );
-
-	await setupWooCommerce();
-} );
+import { test as setup } from '../../utils';
+import { shopConfigGermany } from '../../resources';
 
 // --- Mollie Germany ---
 
-setup( 'setup:mollie:germany;', async ( { utils, mollieApi } ) => {
-	await utils.configureStore( shopSettings.germany.general );
+setup( 'setup:mollie;', async ( { utils } ) => {
+	await utils.configureStore( shopConfigGermany );
 	await utils.installAndActivateMollie();
-	await mollieApi.setMollieApiKeys( mollieApiKeys.default );
-	await mollieApi.cleanMollieDb();
-	await mollieApi.setMollieApiKeys( mollieApiKeys.default );
+	await utils.cleanReconnectMollie();
 } );
 
-// --- Checkout layout ---
+// --- Setup specific Mollie API (assumes Mollie is already installed) ---
 
-setup( 'setup:checkout:block;', async ( { utils } ) => {
-	await utils.configureStore( { enableClassicPages: false } );
+setup( 'setup:payment-api;', async ( { mollieApi } ) => {
+	await mollieApi.setAdvancedSettings( {
+		apiMethod: 'payment',
+	} );
 } );
 
-setup( 'setup:checkout:classic;', async ( { utils } ) => {
-	await utils.configureStore( { enableClassicPages: true } );
-} );
-
-// --- Tax ---
-
-setup( 'setup:tax:inc;', async ( { utils } ) => {
-	await utils.configureStore( { taxes: taxSettings.including } );
-} );
-
-setup( 'setup:tax:exc;', async ( { utils } ) => {
-	await utils.configureStore( { taxes: taxSettings.excluding } );
+setup( 'setup:order-api;', async ( { mollieApi } ) => {
+	await mollieApi.setAdvancedSettings( {
+		apiMethod: 'order',
+	} );
 } );

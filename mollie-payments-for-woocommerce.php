632dd41af444422ead1b2e47f0a3220a6323d48f
syde-mollie-deploybot <mollie-deploybot@syde.com>
2026-04-15 12:56:46 +0000

    [BOT] Add build artifact from dev/qa/refactor-env-setup -> qa/refactor-env-setup.

--- mollie-payments-for-woocommerce.php
+++ mollie-payments-for-woocommerce.php
@@ -4,7 +4,7 @@
  * Plugin Name: Mollie Payments for WooCommerce
  * Plugin URI: https://www.mollie.com
  * Description: Accept payments in WooCommerce with the official Mollie plugin
- * Version: v8.1.6+qa-refactor-env-setup.376e4e2
+ * Version: v8.1.6+qa-refactor-env-setup.8e003b5
  * Author: Mollie
  * Author URI: https://www.mollie.com
  * Requires at least: 5.0

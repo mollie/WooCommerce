c5f3f22a24b5b46902b0cffe6d33441c8be0821d
syde-mollie-deploybot <mollie-deploybot@syde.com>
2026-04-15 11:00:56 +0000

    [BOT] Add build artifact from dev/PIWOO-792-paypal-filter-by-virtual-product-type -> PIWOO-792-paypal-filter-by-virtual-product-type.

--- public/js/mollieBlockIndex.min.asset.php
+++ public/js/mollieBlockIndex.min.asset.php
@@ -2,4 +2,4 @@
 
 namespace Mollie;
 
-return array('dependencies' => array('react', 'wc-settings', 'wp-data', 'wp-element', 'wp-hooks'), 'version' => 'f9cb9eab35d2538cc2a3');
+return array('dependencies' => array('react', 'wc-settings', 'wp-data', 'wp-element', 'wp-hooks'), 'version' => '3b041137baf12fb491d4');

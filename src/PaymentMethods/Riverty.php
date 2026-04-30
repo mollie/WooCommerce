2d9d99c3e95ea540691d06534a1f96eccb35865b
syde-mollie-deploybot <mollie-deploybot@syde.com>
2026-04-30 11:49:52 +0000

    [BOT] Add build artifact from dev/qa/ci-test-reporting -> qa/ci-test-reporting.

--- src/PaymentMethods/Riverty.php
+++ src/PaymentMethods/Riverty.php
@@ -1,36 +1,15 @@
 <?php
 
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace Mollie\WooCommerce\PaymentMethods;
 
 use Automattic\WooCommerce\StoreApi\Exceptions\RouteException;
-
-class Riverty extends AbstractPaymentMethod implements PaymentMethodI
+class Riverty extends \Mollie\WooCommerce\PaymentMethods\AbstractPaymentMethod implements \Mollie\WooCommerce\PaymentMethods\PaymentMethodI
 {
     protected function getConfig(): array
     {
-        return [
-            'id' => 'riverty',
-            'defaultTitle' => 'Riverty',
-            'settingsDescription' => 'To accept payments via Riverty, all default WooCommerce checkout fields should be enabled and required.',
-            'defaultDescription' => '',
-            'paymentFields' => true,
-            'additionalFields' => ['birthdate', 'phone'],
-            'instructions' => false,
-            'supports' => [
-                'products',
-                'refunds',
-            ],
-            'filtersOnBuild' => false,
-            'confirmationDelayed' => false,
-            'paymentCaptureMode' => 'manual',
-            'phonePlaceholder' => 'Please enter your phone here. +316xxxxxxxx', 'mollie-payments-for-woocommerce',
-            'birthdatePlaceholder' => 'Please enter your birthdate here.', 'mollie-payments-for-woocommerce',
-            'docs' => 'https://www.mollie.com/gb/payments/riverty',
-        ];
+        return ['id' => 'riverty', 'defaultTitle' => 'Riverty', 'settingsDescription' => 'To accept payments via Riverty, all default WooCommerce checkout fields should be enabled and required.', 'defaultDescription' => '', 'paymentFields' => \true, 'additionalFields' => ['birthdate', 'phone'], 'instructions' => \false, 'supports' => ['products', 'refunds'], 'filtersOnBuild' => \false, 'confirmationDelayed' => \false, 'paymentCaptureMode' => 'manual', 'phonePlaceholder' => 'Please enter your phone here. +316xxxxxxxx', 'mollie-payments-for-woocommerce', 'birthdatePlaceholder' => 'Please enter your birthdate here.', 'mollie-payments-for-woocommerce', 'docs' => 'https://www.mollie.com/gb/payments/riverty'];
     }
-
     // Replace translatable strings after the 'after_setup_theme' hook
     public function initializeTranslations(): void
     {
@@ -38,15 +17,11 @@ class Riverty extends AbstractPaymentMethod implements PaymentMethodI
             return;
         }
         $this->config['defaultTitle'] = __('Riverty', 'mollie-payments-for-woocommerce');
-        $this->config['settingsDescription'] = __(
-            'To accept payments via Riverty, all default WooCommerce checkout fields should be enabled and required.',
-            'mollie-payments-for-woocommerce'
-        );
+        $this->config['settingsDescription'] = __('To accept payments via Riverty, all default WooCommerce checkout fields should be enabled and required.', 'mollie-payments-for-woocommerce');
         $this->config['phonePlaceholder'] = __('Please enter your phone here. +316xxxxxxxx', 'mollie-payments-for-woocommerce');
         $this->config['birthdatePlaceholder'] = __('Please enter your birthdate here.', 'mollie-payments-for-woocommerce');
-        $this->translationsInitialized = true;
+        $this->translationsInitialized = \true;
     }
-
     public function getFormFields($generalFormFields): array
     {
         /**
@@ -58,7 +33,6 @@ class Riverty extends AbstractPaymentMethod implements PaymentMethodI
         if (isset($generalFormFields['hide_order_lines'])) {
             unset($generalFormFields['hide_order_lines']);
         }
-
         return $generalFormFields;
     }
 }

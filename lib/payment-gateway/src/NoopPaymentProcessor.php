ca2eeb858de7ee6c7af969aadfe76027a588bb32
syde-mollie-deploybot <mollie-deploybot@syde.com>
2026-05-05 14:02:42 +0000

    [BOT] Add build artifact from dev/PIWOO-878-playground-activation-fix -> PIWOO-878-playground-activation-fix.

--- lib/payment-gateway/src/NoopPaymentProcessor.php
+++ lib/payment-gateway/src/NoopPaymentProcessor.php
@@ -1,16 +1,12 @@
 <?php
 
-declare(strict_types=1);
-
-namespace Inpsyde\PaymentGateway;
+declare (strict_types=1);
+namespace Mollie\Inpsyde\PaymentGateway;
 
 class NoopPaymentProcessor implements PaymentProcessorInterface
 {
     public function processPayment(\WC_Order $order, PaymentGateway $gateway): array
     {
-        return [
-            'result' => 'success',
-            'redirect' => $gateway->get_return_url($order),
-        ];
+        return ['result' => 'success', 'redirect' => $gateway->get_return_url($order)];
     }
 }

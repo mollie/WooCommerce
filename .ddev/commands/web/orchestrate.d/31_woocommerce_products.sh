02e10d8f241142e82a99291314993539e0def30f
syde-mollie-deploybot <mollie-deploybot@syde.com>
2026-05-08 19:23:13 +0000

    [BOT] Add build artifact from dev/qa/fix-global-setup -> qa/fix-global-setup.

--- .ddev/commands/web/orchestrate.d/31_woocommerce_products.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#ddev-silent-no-warn
-
-wp plugin install wordpress-importer --activate
-wp import ../wordpress/wp-content/plugins/woocommerce/sample-data/sample_products.xml --authors=skip
--- src/SDK/WordPressHttpAdapter.php
+++ src/SDK/WordPressHttpAdapter.php
@@ -1,13 +1,11 @@
 <?php
 
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace Mollie\WooCommerce\SDK;
 
 use Mollie\Api\Exceptions\ApiException;
 use Mollie\Api\Exceptions\CurlConnectTimeoutException;
 use Mollie\Api\HttpAdapter\MollieHttpAdapterInterface;
-
 class WordPressHttpAdapter implements MollieHttpAdapterInterface
 {
     /**
@@ -19,7 +17,6 @@ class WordPressHttpAdapter implements MollieHttpAdapterInterface
      */
     const HTTP_NO_CONTENT = 204;
     const PAYMENT_HTTP_NO_CONTENT = 202;
-
     /**
      * @param string $httpMethod
      * @param string $url
@@ -31,34 +28,22 @@ class WordPressHttpAdapter implements MollieHttpAdapterInterface
     public function send($httpMethod, $url, $headers, $httpBody)
     {
         $headers['Content-Type'] = 'application/json';
-
-        $args = [
-            'method' => $httpMethod,
-            'body' => $httpBody,
-            'headers' => $headers,
-            'user-agent' => $headers['User-Agent'],
-            'sslverify' => true,
-            'timeout' => self::DEFAULT_TIMEOUT,
-        ];
+        $args = ['method' => $httpMethod, 'body' => $httpBody, 'headers' => $headers, 'user-agent' => $headers['User-Agent'], 'sslverify' => \true, 'timeout' => self::DEFAULT_TIMEOUT];
         $response = wp_remote_request($url, $args);
-
         if (is_wp_error($response)) {
-            $message =  $response->get_error_message() ?? 'Unknown error';
+            $message = $response->get_error_message() ?? 'Unknown error';
             $code = is_int($response->get_error_code()) ? $response->get_error_code() : 0;
             // phpcs:disable WordPress.Security.EscapeOutput.ExceptionNotEscaped
             throw new ApiException(esc_html($message), $code);
             // phpcs:enable WordPress.Security.EscapeOutput.ExceptionNotEscaped
         }
-
         return $this->parseResponse($response);
     }
-
     public function versionString()
     {
         global $wp_version;
         return 'WordPress/' . $wp_version;
     }
-
     /**
      * @param $response
      * @throws ApiException
@@ -71,34 +56,25 @@ class WordPressHttpAdapter implements MollieHttpAdapterInterface
             if ($statusCode === self::HTTP_NO_CONTENT || $statusCode === self::PAYMENT_HTTP_NO_CONTENT) {
                 return null;
             }
-
             throw new ApiException(esc_html("No response body found."));
         }
-
         $body = @json_decode($httpBody);
-
         // GUARDS
-        if (json_last_error() !== JSON_ERROR_NONE) {
+        if (json_last_error() !== \JSON_ERROR_NONE) {
             throw new ApiException(esc_html("Unable to decode Mollie response: '{$response}'."));
         }
-
         if (isset($body->error)) {
             throw new ApiException(esc_html($body->error->message));
         }
-
         if ($statusCode >= 400) {
             $message = esc_html("Error executing API call ({$body->status}: {$body->title}): {$body->detail}");
-
             $field = null;
-
-            if (! empty($body->field)) {
+            if (!empty($body->field)) {
                 $field = $body->field;
             }
-
             if (isset($body->_links, $body->_links->documentation)) {
                 $message .= ". Documentation: {$body->_links->documentation->href}";
             }
-
             if ($httpBody) {
                 $message .= ". Request body: {$httpBody}";
             }
@@ -106,7 +82,6 @@ class WordPressHttpAdapter implements MollieHttpAdapterInterface
             throw new ApiException(esc_html($message), $statusCode, esc_html($field));
             // phpcs:enable WordPress.Security.EscapeOutput.ExceptionNotEscaped
         }
-
         return $body;
     }
 }

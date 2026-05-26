4d901bf275de9d05fe9b0868192d87098f721380
syde-mollie-deploybot <mollie-deploybot@syde.com>
2026-05-26 09:11:57 +0000

    [BOT] Add build artifact from dev/PIWOO-882-woo-commerce-tracks-integration-mollie-payments-plugin-phase-1 -> PIWOO-882-woo-commerce-tracks-integration-mollie-payments-plugin-phase-1.

--- src/Tracks/TracksModule.php
+++ src/Tracks/TracksModule.php
@@ -14,11 +14,12 @@ class TracksModule implements ServiceModule, ExecutableModule
     use ModuleClassNameIdTrait;
     public const OPTION_FIRST_TEST_PAYMENT_TRACKED = 'mollie_tracks_first_test_payment_tracked';
     public const OPTION_API_KEYS_VIEWED = 'mollie_tracks_api_keys_viewed';
+    public const OPTION_PLUGIN_ACTIVATED_TRACKED = 'mollie_tracks_plugin_activated_tracked';
     private const OPTION_PLUGIN_ACTIVATED = 'mollie_tracks_plugin_activated';
     private const OPTION_FIRST_TEST_PAYMENT_PENDING = 'mollie_tracks_first_test_payment_pending';
     private const OPTION_PREFIX = 'mollie-payments-for-woocommerce_';
     private const PAYMENT_MODE_TEST = 'test';
-    private const ALL_TRACKING_OPTIONS = [self::OPTION_FIRST_TEST_PAYMENT_TRACKED, self::OPTION_FIRST_TEST_PAYMENT_PENDING, self::OPTION_API_KEYS_VIEWED, self::OPTION_PLUGIN_ACTIVATED];
+    private const ALL_TRACKING_OPTIONS = [self::OPTION_FIRST_TEST_PAYMENT_TRACKED, self::OPTION_FIRST_TEST_PAYMENT_PENDING, self::OPTION_API_KEYS_VIEWED, self::OPTION_PLUGIN_ACTIVATED_TRACKED, self::OPTION_PLUGIN_ACTIVATED];
     public function services(): array
     {
         return [\Mollie\WooCommerce\Tracks\TracksEventRecorder::class => static function (ContainerInterface $container): \Mollie\WooCommerce\Tracks\TracksEventRecorder {
@@ -80,10 +81,14 @@ class TracksModule implements ServiceModule, ExecutableModule
                 return;
             }
             delete_option(self::OPTION_PLUGIN_ACTIVATED);
+            if (get_option(self::OPTION_PLUGIN_ACTIVATED_TRACKED)) {
+                return;
+            }
             // Skip if merchant is already connected to Mollie
             if ($settingsHelper->getConnectionStatus()) {
                 return;
             }
+            update_option(self::OPTION_PLUGIN_ACTIVATED_TRACKED, '1', \false);
             $recorder->recordEvent('mollie_plugin_activated');
         }, 20);
     }

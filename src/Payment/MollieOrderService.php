3405118e53f933af6ed15ea1fc67644d340bcd8d
Sai Asish Y <say.apm35@gmail.com>
2026-07-18 11:36:27 -0700

    Return empty string when payment has no redirect URL in webhook fallback
    Signed-off-by: Sai Asish Y <say.apm35@gmail.com>

--- src/Payment/MollieOrderService.php
+++ src/Payment/MollieOrderService.php
@@ -1022,6 +1022,6 @@ class MollieOrderService
             $this->logger->debug(__METHOD__ . ": payment $transactionID not found.", [true]);
             return '';
         }
-        return $payment->redirectUrl;
+        return $payment->redirectUrl ?? '';
     }
 }

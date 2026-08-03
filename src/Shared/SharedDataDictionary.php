09e332a2e7e5ebb3ba3ad1b57dc946115977aaa3
syde-mollie-deploybot <mollie-deploybot@syde.com>
2026-08-03 11:23:37 +0000

    [BOT] Add build artifact from dev/develop -> develop.

--- src/Shared/SharedDataDictionary.php
+++ src/Shared/SharedDataDictionary.php
@@ -46,7 +46,7 @@ class SharedDataDictionary
     /**
      * @var string[]
      */
-    public const ALLOWED_LANGUAGE_CODES = ['en_US', 'nl_NL', 'nl_BE', 'fr_FR', 'fr_BE', 'de_DE', 'de_AT', 'de_CH', 'es_ES', 'ca_ES', 'pt_PT', 'it_IT', 'nb_NO', 'sv_SE', 'fi_FI', 'da_DK', 'is_IS', 'hu_HU', 'pl_PL', 'lv_LV', 'lt_LT'];
+    public const ALLOWED_LANGUAGE_CODES = ['en_US', 'nl_NL', 'nl_BE', 'fr_FR', 'fr_BE', 'de_DE', 'de_AT', 'de_CH', 'es_ES', 'ca_ES', 'pt_PT', 'it_IT', 'nb_NO', 'sv_SE', 'fi_FI', 'da_DK', 'is_IS', 'hu_HU', 'pl_PL', 'lv_LV', 'lt_LT', 'cs_CZ', 'sk_SK'];
     /**
      * @var string
      */

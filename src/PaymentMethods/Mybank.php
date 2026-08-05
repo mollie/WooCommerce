93366a5c52a49b6572c22fcda0d8fb90188d3414
syde-mollie-deploybot <mollie-deploybot@syde.com>
2026-08-05 13:36:11 +0000

    [BOT] Add build artifact from dev/PIWOO-920-webhook-double-payment-complete-issue -> PIWOO-920-webhook-double-payment-complete-issue.

--- src/PaymentMethods/Mybank.php
+++ src/PaymentMethods/Mybank.php
@@ -1,30 +1,14 @@
 <?php
 
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace Mollie\WooCommerce\PaymentMethods;
 
-class Mybank extends AbstractPaymentMethod implements PaymentMethodI
+class Mybank extends \Mollie\WooCommerce\PaymentMethods\AbstractPaymentMethod implements \Mollie\WooCommerce\PaymentMethods\PaymentMethodI
 {
     protected function getConfig(): array
     {
-        return [
-            'id' => 'mybank',
-            'defaultTitle' => 'MyBank',
-            'settingsDescription' => 'To accept payments via MyBank',
-            'defaultDescription' => '',
-            'paymentFields' => false,
-            'instructions' => true,
-            'supports' => [
-                'products',
-                'refunds',
-                ],
-            'filtersOnBuild' => false,
-            'confirmationDelayed' => false,
-            'docs' => '',
-        ];
+        return ['id' => 'mybank', 'defaultTitle' => 'MyBank', 'settingsDescription' => 'To accept payments via MyBank', 'defaultDescription' => '', 'paymentFields' => \false, 'instructions' => \true, 'supports' => ['products', 'refunds'], 'filtersOnBuild' => \false, 'confirmationDelayed' => \false, 'docs' => ''];
     }
-
     public function initializeTranslations(): void
     {
         if ($this->translationsInitialized) {
@@ -32,9 +16,8 @@ class Mybank extends AbstractPaymentMethod implements PaymentMethodI
         }
         $this->config['defaultTitle'] = __('MyBank', 'mollie-payments-for-woocommerce');
         $this->config['settingsDescription'] = __('To accept payments via MyBank', 'mollie-payments-for-woocommerce');
-        $this->translationsInitialized = true;
+        $this->translationsInitialized = \true;
     }
-
     public function getFormFields($generalFormFields): array
     {
         return $generalFormFields;

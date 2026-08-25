2b1035a36cf06a3e1091caae7c9c4e628ff98ed7
Kyrylo <k.braslavskyi@syde.com>
2026-08-25 10:53:55 +0300

    Ensure we are comparing values of the same type

--- src/Payment/MollieOrderService.php
+++ src/Payment/MollieOrderService.php
@@ -122,7 +122,7 @@ class MollieOrderService
 
         $order = $orders[0];
 
-        if ($order->get_id() !== (int) $order_id) {
+        if ((int) $order->get_id() !== (int) $order_id) {
             $this->httpResponse->setHttpResponseCode(401);
             $this->logger->debug(__METHOD__ . ":  found order {$order->get_id()} is not the same as provided order $order_id.");
             return;
